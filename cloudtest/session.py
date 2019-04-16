76656973215c00ee97ed7649fe3ad7c07a0672a7
Darnell Martin <darnell.martin@sagenet.com>
2019-04-15 22:10:48 -0400

    Removed dead code

--- cloudtest/session.py
+++ cloudtest/session.py
@@ -8,15 +8,10 @@ class Session(object):
         self.profile = profile
         self.backend = cloudtest.backend.aws.AWS(self.profile)
 
-
-
     def __getattr__(self, name):
-        #assert name in testinfra.modules.modules, name + " is not a module"
         module_class = cloudtest.modules.get_module_class(name)
         module_class.backend = self.backend
-        #obj = module_class.get_module(self)
         obj = module_class
-        #module_class()
         setattr(self, name, obj)
         return obj
 
