4d48be15bd17946339f243b02d67722e5850f6c4
Darnell Martin <darnell.martin@sagenet.com>
2019-04-26 02:01:23 -0400

    Added support for EC2 Module and attributes

--- cloudtest/modules/ec2instance.py
+++ cloudtest/modules/ec2instance.py
@@ -5,38 +5,75 @@ class EC2Instance(Module):
     """ Test EC2 Instances """
 
     def __init__(self, name=None, id=None):
-        ec2 = session.client('ec2')
+        super()
+        ec2 = self.backend.session.client('ec2')
+        self.name = name
+        self.id = id
         if name is not None:
-            self.name = name
+            self.instance = ec2.describe_instances(
+            Filters=[
+            {
+                'Name': 'tag:Name',
+                'Values': [name]
+            }])
         elif id is not None:
-            self.id = id
+            self.instance = ec2.describe_instances(InstanceIds=[self.id])
         else:
+            print("ERROR")
             pass
             #This is where an error needs thrown
 
+        if len(self.instance["Reservations"][0]["Instances"]) > 0:
+            self.instance_data = self.instance["Reservations"][0]["Instances"][0]
+        else:
+            self.instance_data = None
+
     def exists(self):
-        pass
+        if self.instance_data is not None:
+            return True
+        else:
+            return False
 
     def state_equals(self, state):
-        pass
+        if self.instance_data["State"]["Name"] == state:
+            return True
+        else:
+            return False
 
     def tag_equals(self, key, value):
-        pass
+        check = list(filter(lambda x: (x["Key"] == key and x["Value"] == value), data["Tags"]))
+        if len(check) > 0:
+            return True
+        else:
+            return False
 
     def tag_exists(self, key):
-        pass
+        check = list(filter(lambda x: (x["Key"] == key), data["Tags"]))
+        if len(check) > 0:
+            return True
+        else:
+            return False
 
     def type_equals(self, type):
-        pass
+        if self.instance_data["InstanceType"]== type:
+            return True
+        else:
+            return False
 
     def az_equals(self, az):
-        pass
+        if self.instance_data["AvailabilityZone"]== az:
+            return True
+        else:
+            return False
 
     def keypair_equals(self, keypair):
-        pass
+        if self.instance_data["KeyName"]== keypair:
+            return True
+        else:
+            return False
 
     def owner_equals(self, owner):
-        pass
-
-    def has_securitygroup(self,sg_id):
-        pass
+        if self.instance_data["OwnerId"]== owner:
+            return True
+        else:
+            return False
